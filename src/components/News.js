483e27e960b238dad35b83b8ce550ea36ec25621
AliBinNaseer <AliIsWorking459@gmail.com>
2021-11-11 17:47:40 +0500

    News API + Cards

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,41 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     render() {
         return (
-            <div>
-                <NewsItem/>
-
+            <div className="container my-2">
+                <div className="row">
+                    <div className="col-md-4">
+                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
+                    </div>
+                </div>
+                
                 {/* 8d91428a0f0344cd8bcd71463e79bb37 */}
             </div>
         )
