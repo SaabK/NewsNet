9b669da6f4c56ab9ba189f1a197833f9f9afd00c
AliBinNaseer <AliIsWorking459@gmail.com>
2021-11-14 15:30:42 +0500

    News API Usage + Pages

--- src/components/News.js
+++ src/components/News.js
@@ -2,43 +2,69 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            articles : [],
+            loading: false,
+            page: 1
+        }
+    }
+
+    async componentDidMount(){
+        let url = "https://newsapi.org/v2/everything?q=cricket&apiKey=8d91428a0f0344cd8bcd71463e79bb37&page=1&pageSize=18";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            articles: parsedData.articles,
+            totalArticles: parsedData.totalResults
+        });
+    }
+
+    handlePrevClick = async () => {
+        let url = `https://newsapi.org/v2/everything?q=cricket&apiKey=8d91428a0f0344cd8bcd71463e79bb37&page=${this.state.page-1}&pageSize=18`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            articles: parsedData.articles,
+            page: this.state.page - 1
+        });
+    }
+
+    handleNextClick = async () => {
+        if(this.state.page + 1 > Math.ceil(this.state.totalResults/18)){
+            
+        }
+        else{
+            let url = `https://newsapi.org/v2/everything?q=cricket&apiKey=8d91428a0f0344cd8bcd71463e79bb37&page=${this.state.page+1}&pageSize=18`;
+            console.log(url)
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({
+                articles: parsedData.articles,
+                page: this.state.page + 1
+            });
+        }
+    }
+
     render() {
         return (
             <div className="container my-2">
+                <h1 className="mx-4">NewsNet - Top Headlines</h1>
                 <div className="row">
-                    <div className="col-md-4">
-                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
-                    </div>
+                    {this.state.articles.map((element)=>{
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title.slice(0, 40)+"..."} imageUrl={element.urlToImage} description={element.description.slice(0, 85)+"..."} newsUrl={element.url}/>
+                        </div>
+                    })}
                 </div>
-                <div className="row">
-                    <div className="col-md-4">
-                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
-                    </div>
+
+                <div className="container d-flex justify-content-between">
+                    <button type="button" disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
-                <div className="row">
-                    <div className="col-md-4">
-                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="Title Right Here..." description="All of the description about this specif news goes right here.."/>
-                    </div>
-                </div>
-                
                 {/* 8d91428a0f0344cd8bcd71463e79bb37 */}
             </div>
         )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let {title, description} = this.props;
+        let {title, description, imageUrl, newsUrl} = this.props;
+
+        let picDim = {
+            width: "286px",
+            height: "147px"
+        }
         return (
             <div>
                 <div className="card mx-4 my-3" style={{width: "18rem"}}>
-                    <img src="..." className="card-img-top" alt="..."/>
+                    <img style={picDim} src={imageUrl===null?"https://howfix.net/wp-content/uploads/2018/02/sIaRmaFSMfrw8QJIBAa8mA-article.png":imageUrl} className="card-img-top" alt="..."/>
                     <div className ="card-body">
-                    <h5 className ="card-title">{title}</h5>
-                    <p className ="card-text">{description}</p>
-                    <a href="/" className ="btn btn-primary">Go somewhere</a>
+                    <h5 className ="card-title">{title===null?"Title not found":title}</h5>
+                    <p className ="card-text">{description===null?"Never gonna give u up, description not found":description}</p>
+                    <a href={newsUrl===null?"https://www.youtube.com/watch?v=dQw4w9WgXcQ":newsUrl} rel="noreferrer" target="_blank" className ="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
